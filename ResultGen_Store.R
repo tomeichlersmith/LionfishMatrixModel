3650e07572f8f25e8c5645ade0c0e47a28f428a6
Tom Eichlersmith <tom.eichlersmith@gmail.com>
2017-10-12 17:36:09 -0500

    Code Operational, running it on realistic numbers now

--- ResultGen_Store.R
+++ ResultGen_Store.R
@@ -9,19 +9,29 @@ source("MatrixModelCSim.R")
 
 #Statements defining the lists of possible values for the inputs
 # Any inputs not defined here will just be defined as their default values
-crnumberOpts <- c(10,100,1000)
-crlengthOpts <- c(1,6,12,18,24,36,48,60,120)
-hnumberOpts <- c(10,100,1000)
-hlengthOpts <- c(12,18,24,36,48,60,120,1200)
+crnumberOpts <- seq(1,5001,50)
+crlengthOpts <- seq(1,601,12)
+hnumberOpts <- c(100,500,1000)
+hlengthOpts <- c(12,60,120,1200)
 nheOpts <- c(0,0.001,0.005,0.01,0.05,0.1,0.15,0.2)
 mortslopeOpts <- seq(0,2*dMortSlope,(2*dMortSlope)/10)
 
 numbertrials <- length(crnumberOpts)*length(crlengthOpts)*length(hnumberOpts)*length(hlengthOpts)*length(nheOpts)*length(mortslopeOpts)
 
 #File Setup
-titlerow <- data.frame(
-  name = 
-)
+# Only evaluate if creating a new output file i.e. let setupfile = TRUE if you want to overwrite
+#  a previous file or create a new file with a new name, otherwise let setupfile = FALSE
+outputfile <- "DUMMYNAMETHATISNOTREALATALL.csv"
+setupfile <- TRUE
+if (setupfile) {
+  titlerow <- c("CRNumber","CRLength","HNumber","HLength","NHE","MortSlope","EradTime")
+  write(
+    titlerow,
+    file = outputfile,
+    ncolumns = length(titlerow),
+    sep = ","
+  )
+}
 
 for (crnum in crnumberOpts) {
   for (crlen in crlengthOpts) {
@@ -41,16 +51,14 @@ for (crnum in crnumberOpts) {
               shortout = TRUE)
             
             #Output results to file
-            singlerow <- data.frame(
-              names=c("CRNumber","CRLength","HNumber","HLength","NHE","MortSlope","EradTime"),
-              a=c(crnum,crlen,hnum,hlen,nhe,mslope,results["EradTime"])
-              ) #Create data.frame for the simulated row
-            write.table(
+            singlerow <- c(
+              crnum , crlen , hnum , hlen , nhe , mslope , results["EradTime"]
+              )
+            write(
               singlerow,
-              "DUMMYNAMETHATISNTREAL",
-              row.names = FALSE, col.names = FALSE,
+              outputfile,
+              ncolumns = length(singlerow),
               sep = ",",
-              quote = FALSE,
               append = TRUE
               )
           }
